c70dbb039d8afd8233eb80af244202ed7dd1511f
joe <joe@joeth.uk>
2020-09-15 16:55:26 +0100

    initial commit

---
+++ lesson-1/bobsbricks.c
@@ -0,0 +1,38 @@
+// Bob's Bricks
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int wallNum, wallHeight, wallLength, num = 0;
+    
+    // Gather input for calculations
+    printf("\nHow long are the walls? : ");
+    fflush(stdin); scanf("%d", &wallLength);
+    
+    printf("\nHow many bricks tall are the walls? : ");
+    fflush(stdin); scanf("%d", &wallHeight);
+
+    printf("\nHow many walls? : ");
+    fflush(stdin); scanf("%d", &wallNum);
+
+    // Calculate number of bricks
+    float brickNum_unrounded = wallLength * 1.5 * wallHeight * wallNum;
+    // Round up number of bricks
+    int brickNum = (int)(brickNum_unrounded < 0 ? (brickNum_unrounded) : (brickNum_unrounded + 0.5));
+    // Calculate number of pallets
+    int palletNum = (float)brickNum / 100 + 1;
+
+    if(brickNum == 0){
+        palletNum = 0;
+        printf("Why do you need pallets if there are no bricks?");
+    }
+
+    printf("\n bricks needed: %d\n", brickNum);
+
+    printf("\n pallets needed: %d\n",palletNum);
+
+    printf("%d", num);
+    return 0;
+}
